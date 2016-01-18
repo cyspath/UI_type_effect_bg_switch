c8967729dc82de44a766be17d752dd9d5a99cc75
Mike <lvlichaelly@gmail.com>
2016-01-18 09:50:27 -0800

    added files

---
+++ README.md
@@ -0,0 +1,1 @@
+### Auto typing captions, and images will fade out and fade in after upon typing completion.
---
+++ main.js
@@ -0,0 +1,6 @@
+var text = [
+  "Strolling up along a rocky mountain pass",
+  "In the shire of ‘Nowhere’",
+  "To a world so green, so very full of grass.",
+  "In a town called ‘Someplace’."
+]
