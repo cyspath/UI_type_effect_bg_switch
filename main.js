fca672eb5bc541151b8d04c7720fa3dd92baf692
Mike <lvlichaelly@gmail.com>
2016-01-18 17:58:13 -0800

    added special case of last line typed

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function type(i) {
     }
     text += arr[idx]
     idx += 1
-    $("p").html(text)
+    $(".caption-text").html(text)
   }.bind(this), 50)
 
 }
@@ -38,5 +38,45 @@ $(document).ready(function () {
   $('button').click(function() {
     location.reload();
   })
+
+
+
+
+  var slideCaptions = ["First Line", "Second Line", "Third Line", "Last Line will have this type effect"]
+$('.typewriter-front').hide()
+
+
+function titleCaptions(i) {
+    console.log("n");
+    if (i == 3) {
+      var arr = slideCaptions[i].split("");
+      var text = ""
+      var idx = 0
+
+      var interval = setInterval(function () {
+        if (idx == arr.length - 1) {
+          clearInterval(interval)
+        }
+        text += arr[idx]
+        idx += 1
+        $('.typewriter-front').html(text)
+      }.bind(this), 100)
+    }
+
+    $('.typewriter-front').html(slideCaptions[i])
+    $('.typewriter-front').fadeIn(500, function() {
+      setTimeout(function() {
+        if (i < 3) {
+          $('.typewriter-front:visible').fadeOut(500, function() {
+              titleCaptions(i + 1)
+          }.bind(this))
+        }
+      }.bind(this), 2000)
+    }.bind(this))
+  }
+
+   setTimeout(function() {
+    titleCaptions(0)
+   }.bind(this), 300)
 
 })
